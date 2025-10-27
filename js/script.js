2ad6ba254ee3087bef50da02281f5e574c07904d
Anubhav Kumar <144251654+Anubhavmishra07@users.noreply.github.com>
2025-10-27 18:59:08 +0530

    intial commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
+"use strict";
 // let js = "amazing";
 // if (js === "amazing") alert("js is fun!");
 // let PI = 4;
 // booltype = false;
 // console.log(booltype);
-booltype = 10;
-console.log(typeof {});
+// booltype = 10;
+// console.log(typeof {});
+// function hel(x, y)
+// {console.log('hello');}
+// hel();
+// let height;
+// if (height !== NaN) {
+//   console.log('Height is defined');
+// } else {
+//   console.log('Height is UNDEFINED');
+// }
+// const calcAge3 = birthYear => 2037 - birthYear;
+
+// console.log(calcAge3(1991) );
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+console.log(jonas.lastName);
