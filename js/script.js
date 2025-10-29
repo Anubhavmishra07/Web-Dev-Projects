11e08894716970f9675ec891fdf0cd3ca8bf81eb
Anubhav Kumar <144251654+Anubhavmishra07@users.noreply.github.com>
2025-10-29 18:36:00 +0530

    intial commit

--- js/script.js
+++ js/script.js
@@ -18,11 +18,37 @@
 // const calcAge3 = birthYear => 2037 - birthYear;
 
 // console.log(calcAge3(1991) );
-const jonas = {
-  firstName: "Jonas",
-  lastName: "Schmedtmann",
-  age: 2037 - 1991,
-  job: "teacher",
-  friends: ["Michael", "Peter", "Steven"],
-};
-console.log(jonas.lastName);
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   age: 2037 - 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"]
+// };
+// // console.log(jonas.lastName);
+//  const interestedIn = prompt('What do you want to know about Jonas?'); 
+
+// console.log(jonas[interestedIn]); 
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+/* Write your code below. Good luck! 🙂 */
+const bills =[22,295,176,440,37,105,10,1100,86,52];
+const tips =[];
+const totals =[];
+for(let i=0;i<bills.length;i++)
+{tips.push(calcTip(bills[i]));
+totals.push(bills[i]+tips[i]);
+}
+console.log(bills);
+console.log(tips);
+console.log(totals);
+
+function calcAverage(totals)
+{    let sum=0; 
+    for(let i=0;i<totals.length;i++)
+    {sum+=totals[i];}
+    return sum/totals.length;
+}
+console.log(calcAverage(totals));
